f8ac47cb4de15ccd42fc81e3072991b4a21dec42
brenda barraza <brendabarraza99@gmail.com>
2024-12-12 23:20:36 -0300

    add models

---
+++ blog/crudphp/Models/Posts.swift
@@ -0,0 +1,11 @@
+
+
+import Foundation
+
+struct Posts: Codable {
+    let id : String
+    let titulo : String
+    let contenido : String
+    let imagen : String
+    let nombre_imagen : String
+}
